f67049a186268dfe805317728e55b09a4a6b83c5
tim19950224 <tim19950224@gmail.com>
2022-11-03 20:00:47 +0800

    tim 2022/11/3  V2

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,5 +1,5 @@
 import Control from "../common/user/userControl.js"
-
+/*
 describe('Top bar', function() {
   
     beforeEach( function() {     
@@ -93,9 +93,12 @@ describe('Menu bar', function() {
     //   })
     // })
   // })
-  //--------------------------------------------------------------------------------------
 
-  describe('empty spec', () => {
+
+
+  //-------------------------------------------------------------------------------------- Tim maintain
+
+  describe('Tim maintain', () => {
   
     it('Models', () => {
     cy.visit('https://www.tm-robot.com/en/')
@@ -118,9 +121,9 @@ describe('Menu bar', function() {
     cy.url().should('eq', 'https://www.tm-robot.com/en/tm20/')
   })
 
-  it('Footer_TM ROBOT', () => {
+   it('AI Cobot', () => {
     //cy.url().should('eq', '')
-    cy.visit('https://www.tm-robot.com/en/')
+     cy.visit('https://www.tm-robot.com/en/')
     cy.get('#elementor-tab-title-1731 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
     cy.get('#elementor-tab-content-1731 > ul > :nth-child(1) > a').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/tm5-700/')
@@ -142,37 +145,87 @@ describe('Menu bar', function() {
     cy.get('#elementor-tab-title-1731 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
     cy.get('#elementor-tab-content-1731 > ul > :nth-child(7) > a').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/mobile-series/')
-  })
+    cy.get('#elementor-tab-title-1732 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-1732 > ul > :nth-child(1) > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-ai-aoi-edge/')
+    cy.get('#elementor-tab-title-1732 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-1732 > ul > :nth-child(2) > a').click()
+    // cy.url().should('eq', 'https://www.tm-robot.com/en/tm-3dvision/')
+   })
+  it('Solution & Services', () => {
+    cy.get('#elementor-tab-title-7271 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-7271 > ul > :nth-child(1) > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-ai-training-server/')
+    cy.get('#elementor-tab-title-7271 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-7271 > ul > :nth-child(2) > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-ai-aoi-edge/')
+    cy.get('#elementor-tab-title-7271 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-7271 > ul > :nth-child(3) > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-image-manager/')
 
-  it('Case Sharing & Traing_Support', () => {
-    cy.visit('https://www.tm-robot.com/en/')
+    cy.get('#elementor-tab-title-7272 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-7272 > ul > li > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-palletizing-solutions/')
+
+    cy.get('#elementor-tab-title-7273 > .elementor-toggle-icon > .elementor-toggle-icon-closed > .fas').click()
+    cy.get('#elementor-tab-content-7273 > ul > li > a').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-plugandplay/')
+    })
+  it('Case Sharing', () => {
+   // cy.visit('https://www.tm-robot.com/en/')
     cy.get('.elementor-element-23905acc > .elementor-widget-container > .elementor-icon-list-items > :nth-child(1) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/video-application/')
     cy.get('.elementor-element-23905acc > .elementor-widget-container > .elementor-icon-list-items > :nth-child(2) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/industry/')
     cy.get('.elementor-element-23905acc > .elementor-widget-container > .elementor-icon-list-items > :nth-child(3) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/view-case-study-look/')
+  })
+
+  it('Traing_Support', () => {
     cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(1) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/contact-us/')
     cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(2) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/download-center/')
     cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(3) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/docs/')
-
-   
-    
-
+    cy.visit('https://www.tm-robot.com/en/')
+    cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(4) > a > .elementor-icon-list-text').click() 
+    cy.url().should('eq', 'https://academic.tm-robot.com/')
+    cy.visit('https://www.tm-robot.com/en/')
     cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(5) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/tm-academic/')
     cy.get('.elementor-element-280e338 > .elementor-widget-container > .elementor-icon-list-items > :nth-child(6) > a > .elementor-icon-list-text').click()
     cy.url().should('eq', 'https://www.tm-robot.com/en/partner-area/')
-    
   })
   
-  it('test', () => {
+  it('About Us', () => {
     cy.visit('https://www.tm-robot.com/en/')
-    cy.get(Command['123']).click() 
-    cy.url().should('eq', 'https://academic.tm-robot.com/')// Oline Training   eq error
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(1) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/about-us/')
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(2) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/company-profile/')
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(3) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/search-for-distributor/') //Search for Distributor   eq Error
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(4) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/tm-news/')
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(5) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/events-archive/') //No en
+    cy.get('.elementor-element-51bbb9f > .elementor-widget-container > .elementor-icon-list-items > :nth-child(6) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/techman-x-omron/')
+    cy.get(':nth-child(7) > a > .elementor-icon-list-text').click()
+    cy.url().should('eq', 'https://www.tm-robot.com/en/media-library/')
   })
 
+//   it('test', () => {
+   
+//   })
+
 })
+
+
+// describe('Today Maintain', () => {
+
+//   it('test', () => {
+
+//     })
+//   })
