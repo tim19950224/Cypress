6d572f9be78d7321cc08da36245cafa7cd0d7f59
AllenLin-web <Allen.Lin@tm-robot.com>
2022-11-03 12:28:45 +0800

    Allen Day1

---
+++ cypress/Common/user/userControl.js
@@ -0,0 +1,19 @@
+// **let**用於宣告一個「只作用在當前區塊的變數」，初始值可選擇性的設定
+
+let Control = {
+    // #### Top bar ####
+    // [TM icon]
+    "TM_icon" : ".elementor-element-0843aa0 > .elementor-column-wrap > .elementor-widget-wrap > .elementor-element > .elementor-widget-container > .raven-widget-wrapper > .raven-site-logo > .raven-site-logo-link > .raven-site-logo-desktop",
+    
+    // [Login]
+    "Login" : "#menu-95cc4da > .menu-item-type-post_type > .raven-menu-item",
+
+    // [Language_English]
+    "Language" : "#sm-16673927015117317-1",
+
+    // #### Menu bar ####
+    "AI_cobot" : "#jet-menu-item-72760 > .top-level-link > .jet-menu-item-wrapper",
+
+}
+
+export default Control
